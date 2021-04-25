f5fd414a09d9870e96300428934a4dfc1aeb350a
Jenny <jennyquach.99@hotmail.com>
2021-04-25 13:49:24 -0400

    Updated navbar

--- sportcred/src/App.js
+++ sportcred/src/App.js
@@ -2,7 +2,7 @@ import React, {useState} from "react";
 // import logo from './logo.svg';
 import './App.css';
 import { HashRouter as Router, Link } from "react-router-dom";
-import { AiOutlineMenu, AiOutlineLeft } from "react-icons/ai";
+import { AiOutlineMenu, AiOutlineLeft, AiOutlineSetting } from "react-icons/ai";
 import { Button } from 'reactstrap';
 
 import AppRouter from "./router";
@@ -11,13 +11,20 @@ let NavBar = () => {
   const [opened, setOpened] = useState(false)
   return (
     <div id="navbar">
-      <div className={"body " + ((opened)? "opened":"")}>
-        <h1>SportsCred</h1>
+      <div className={"body flex-container flex-vertical" + ((opened)? " opened":"")}>
+        <div className="flex-primary">
+          <h1>SportsCred</h1>
+          <div>
+            <div className="menu-item active"><Link>Open Court</Link></div>
+            <div className="menu-item"><Link>Trivia</Link></div>
+            <div className="menu-item"><Link>Debates</Link></div>
+            <div className="menu-item"><Link>Picks and Predictions</Link></div>
+          </div>
+        </div>
         <div>
-          <div className="menu-item active"><Link>Open Court</Link></div>
-          <div className="menu-item"><Link>Trivia</Link></div>
-          <div className="menu-item"><Link>Debates</Link></div>
-          <div className="menu-item"><Link>Picks and Predictions</Link></div>
+          <div className="menu-item"><Link><AiOutlineSetting/></Link></div>
+          <div className="menu-item"><Link>Logout</Link></div>  
+          <div className="menu-item spacer"></div>  
         </div>
       </div>
       <Button className={"burger-btn outline " + ((opened)? "opened":"")} 
