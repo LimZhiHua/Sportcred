994fc4ac75addde9a8c3bee87eeb25722a0d41cf
Jenny <jennyquach.99@hotmail.com>
2021-04-25 15:10:04 -0400

    Got navbar to highlight active page

--- sportcred/src/router.js
+++ sportcred/src/router.js
@@ -5,19 +5,24 @@ import { Switch, Route, Redirect } from "react-router-dom";
 import {
   HOME_URL,
   SIGNIN_URL,
-  SIGNUP_URL
+  SIGNUP_URL,
+  COMPONENTS_URL
 } from "./urls";
 
 import Homepage from "./pages/Homepage";
 import Signin from "./pages/Signin";
 import Signup from "./pages/Signup";
 
+import ComponentPage from "./devPages/componentsPage";
+
 const AppRouter = () => (
   <Switch>
-    {/* TODO: */}
+    {/* Prod */}
     <Route exact path={SIGNIN_URL} component={Signin} />
     <Route exact path={SIGNUP_URL} component={Signup} />
     <Route exact path={HOME_URL} component={Homepage} />
+    {/* Dev */}
+    <Route exact path={COMPONENTS_URL} component={ComponentPage} />
   </Switch>
 );
 
--- sportcred/src/urls.js
+++ sportcred/src/urls.js
@@ -1,6 +1,8 @@
 
-
+// Prod
 export const HOME_URL = "/";
 export const SIGNIN_URL = "/signin";
 export const SIGNUP_URL = "/signup";
 
+// Dev
+export const COMPONENTS_URL = "/components"
