bc2b3dba7ddef423a8605b1c86ff0b122e0538d7
Lim Zhi Hua <thisiszhihua@gmail.com>
2021-05-03 19:19:56 -0400

    just trying out them buttons

--- sportcred/src/pages/TriviaLanding.js
+++ sportcred/src/pages/TriviaLanding.js
@@ -1,10 +1,20 @@
 import React from "react";
+import { Button } from "reactstrap";
 import FloatingSection from "../customComponents/FloatingSection";
+import "../App.css";
+import {orangeButton} from "../style.jsx"
 
 const TriviaLanding = () => {
     return (
         <FloatingSection>
             <h1>TriviaLanding TODO</h1>
+            <Button className = "orangeButton">why you no work</Button>
+            <br></br>
+            <Button style = {orangeButton}>this works, but Jenny hates it</Button>
+
+            <br></br>
+            <Button variant = "primary">why you no work</Button>
+
         </FloatingSection>
     )
 }
---
+++ sportcred/src/style.jsx
@@ -0,0 +1,21 @@
+
+
+const orangeButton = {
+    'color': "black",
+    'border-radius': '5px',
+    'text-align': 'center',
+    'width': "75%",
+    "background-color": "orange"
+
+}
+
+const redButton = {
+    'color': "black",
+    'border-radius': '5px',
+    'text-align': 'center',
+    'width': "75%",
+    "background-color": "green"
+}
+
+
+export {orangeButton, redButton}
