0857d589700f018abecd121d29467fae1fa22992
Anmole Bajwa <anmoleb.bajwa@gmail.com>
2021-06-13 01:09:54 -0400

    [newComponenet] button component updates

--- sportcred/src/customComponents/buttons/Buttons.js
+++ sportcred/src/customComponents/buttons/Buttons.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
+import "./Buttons.css";
+import { makeStyles } from '@material-ui/core/styles';
 
-class defaultButton extends React.Component {
-    render() {
-      return (
-          <div>
-          <Button
-           />
-          </div>
-        );
-    }
-  }
-  
-  export default defaultButton;
+const useStyles = makeStyles({
+  root: {
+    background: '#c4c4c4',
+    border: 0,
+    borderRadius: 40,
+    height: '50%',
+    width: '20%',
+    padding: '1em',
+  },
+});
+
+function DefaultButton(props) {
+  const classes = useStyles();
+    return (
+        <div>
+          <Button 
+          className={classes.root}
+          onClick={props.handleClick}>{props.label}</Button>
+        </div>
+      )
+}
+export default DefaultButton;
