9dd0d0b7470977d8e01c47147cc361b3ac92d6c6
Anmole Bajwa <anmoleb.bajwa@gmail.com>
2021-06-13 01:34:11 -0400

    [newComponent] added more buttons and styles

--- sportcred/src/customComponents/buttons/Buttons.js
+++ sportcred/src/customComponents/buttons/Buttons.js
@@ -3,9 +3,9 @@ import Button from '@material-ui/core/Button';
 import "./Buttons.css";
 import { makeStyles } from '@material-ui/core/styles';
 
-const useStyles = makeStyles({
+const useDefaultStyles = makeStyles({
   root: {
-    background: '#c4c4c4',
+    background: '#909090',
     border: 0,
     borderRadius: 40,
     height: '50%',
@@ -14,8 +14,30 @@ const useStyles = makeStyles({
   },
 });
 
-function DefaultButton(props) {
-  const classes = useStyles();
+const useAnswerStyles = makeStyles({
+  root: {
+    background: '#86C232',
+    border: 0,
+    borderRadius: 40,
+    height: '50%',
+    width: '20%',
+    padding: '1em',
+  },
+});
+
+const useDangerStyles = makeStyles({
+  root: {
+    background: '#FF652F',
+    border: 0,
+    borderRadius: 40,
+    height: '50%',
+    width: '20%',
+    padding: '1em',
+  },
+});
+
+export function DefaultButton(props) {
+  const classes = useDefaultStyles();
     return (
         <div>
           <Button 
@@ -24,4 +46,25 @@ function DefaultButton(props) {
         </div>
       )
 }
-export default DefaultButton;
+
+export function AnswerButton(props) {
+  const classes = useAnswerStyles();
+    return (
+        <div>
+          <Button 
+          className={classes.root}
+          onClick={props.handleClick}>{props.label}</Button>
+        </div>
+      )
+}
+
+export function DangerButton(props) {
+  const classes = useDangerStyles();
+    return (
+        <div>
+          <Button 
+          className={classes.root}
+          onClick={props.handleClick}>{props.label}</Button>
+        </div>
+      )
+}
